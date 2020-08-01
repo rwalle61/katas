5be2764471487b5b32d2401810f532de52dfc121
Richard Waller <Richard.Waller@ibm.com>
2020-08-01 20:37:50 +0100

    test: update test stub

--- src/index.test.js
+++ src/index.test.js
@@ -4,8 +4,14 @@ import foo from '.';
  *
  */
 
-describe('foo', () => {
+describe('foo(n)', () => {
   it('returns 1', () => {
     expect(foo()).toEqual(1);
   });
+  test.each([
+    ['1', 1],
+  ])('%s => %i', (input, expected) => {
+    expect(foo(input)).toEqual(expected);
+  });
+
 });
